21dd0d89574db4ee78fe539701af186c1db61868
Andrew Harris <andrew.harris.1999@outlook.com>
2022-04-21 13:44:03 -0500

    Progress with testing

--- test_classes.py
+++ test_classes.py
@@ -1,10 +1,29 @@
-import unittest
+import pytest
+from classes import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class Test:
+    def setup_method(self):
+        self.t1 = Television()
+        self.t2 = Television()
 
+    def teardown_method(self):
+        del self.t1
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_init(self):
+        assert self.t1.get_status() is False
+        assert self.t1.get_channel() == 0
+        assert self.t1.get_volume() == 0
+
+        assert self.t2.get_status() is False
+        assert self.t2.get_channel() == 0
+        assert self.t2.get_volume() == 0
+
+    def test_power(self):
+        self.t1.power()
+        assert self.t1.get_status() is True
+
+        self.t2.power()
+        self.t2.power()
+        assert self.t2.get_status() is False
+
